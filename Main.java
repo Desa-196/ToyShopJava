fe529d611e685a46dbacf463375f59dccbd7d63e
Пономарев Денис <desa-196@yandex.ru>
2023-09-20 13:35:12 +0300

    Добавил сохранение результатов в csv файл

--- Main.java
+++ Main.java
@@ -10,16 +10,29 @@
 файл
  */
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String args[]){
 
         ToyRandom toyRandom = new ToyRandom();
+        SaveCSV saveCSV = new SaveCSV("toy_prize.csv");
         toyRandom.put(new Toy(1, "Конструктор", 10));
         toyRandom.put(new Toy(2, "Машинка", 20));
         toyRandom.put(new Toy(3, "Кукла", 60));
 
+        ArrayList<Toy> arrayToy = new ArrayList<>();
         for(int i=0; i<100; i++){
-            System.out.println(toyRandom.get().getName()); 
+            arrayToy.add(toyRandom.get());
+            System.out.println(arrayToy.get(i).getName()); 
+        }
+
+        try{
+            saveCSV.saveFile(arrayToy);
+        }
+        catch(IOException e){
+            System.out.println("Не удалось сохранить результат. " + e.getMessage());
         }
     }
 }
---
+++ SaveCSV.java
@@ -0,0 +1,22 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SaveCSV {
+    private String file;
+
+    public SaveCSV(String file) {
+        this.file = file;
+    }
+
+    public void saveFile(ArrayList<Toy> arrayToy) throws IOException {
+        try (FileWriter writer = new FileWriter(file, false)){
+        for (Toy toy : arrayToy) {
+            writer.write(String.format("%s;%s", toy.getId(), toy.getName()) + "\n");
+
+        }
+        writer.flush();
+    }
+
+    }
+}
